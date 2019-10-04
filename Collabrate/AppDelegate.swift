9e6e5c18ae9a44294494e07f01feae4fd0d10725
Guowei Mo <guowei.mo@tesco.com>
2019-10-04 13:21:34 +0100

    setup

---
+++ Collabrate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Collabrate
+//
+//  Created by Guowei Mo on 04/10/2019.
+//  Copyright © 2019 Guowei Mo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ Collabrate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collabrate
+//
+//  Created by Guowei Mo on 04/10/2019.
+//  Copyright © 2019 Guowei Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
